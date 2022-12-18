37e0e582c6af19a4dad7c1130753e599ca5a4876
Daron Marsh <daronmarsh41@gmail.com>
2022-12-17 20:28:33 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenItems = document.querySelectorAll('.hidden');
+hiddenItems.forEach((it) => observer.observe(it));
